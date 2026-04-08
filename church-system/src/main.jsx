784bb20ec2de1c86a91a3dfba51baae0cb73e739
zele26 <zelalemfiseha26@gmail.com>
2026-04-08 18:32:35 +0300

    update dashboard

--- church-system/src/main.jsx
+++ church-system/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// We use 'createRoot' directly since that is what we imported above
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
