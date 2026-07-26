29ffaa7f7c90d707abf41648d785ffb6c811fec4
zele26 <zelalemfiseha26@gmail.com>
2026-07-26 15:03:37 +0300

    change the login method to phone number

--- church-server/routes/authRoutes.js
+++ church-server/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, refreshToken, logout, forgotPassword } = require('../controllers/authControllers');
+const { register, login, forgotPassword } = require('../controllers/authControllers');
 
 router.post('/register', register);
 router.post('/signup', register);
 router.post('/login', login);
-router.post('/refresh', refreshToken);
-router.post('/logout', logout);
 router.post('/forgot-password', forgotPassword);
+
+// Refresh and logout are no longer used – removed
 
 module.exports = router;
